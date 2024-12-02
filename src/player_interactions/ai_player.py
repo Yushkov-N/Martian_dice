5676a6cd79ba30e6d7a1cc31cc24c519eec2c8ee
Yushkov-N <RTUniver@yandex.ru>
2024-12-02 21:06:03 +0300

    Внесены мелкие правки по отступам в коде (внешний вид)

--- src/player_interactions/ai_player.py
+++ src/player_interactions/ai_player.py
@@ -1,4 +1,4 @@
-from src.player import Player
+from abc import ABC
 
 from src.dice import Dice
 from src.player_interaction import PlayerInteraction
@@ -7,11 +7,10 @@ import random
 from src.player_interaction import Action
 
 
-class Bot(PlayerInteraction):
-
+class Bot(PlayerInteraction, ABC):
     actions = [act for act in Action]
     action_continue = [Action.REROLL, Action.REROLL, Action.REROLL, Action.REROLL, Action.REROLL, Action.END_GAME_MOVE]
-    action_chouse_dice = [Action.CHOOSE_RAY, Action.CHOOSE_COW, Action.CHOOSE_HUMAN, Action.CHOOSE_CHICKEN]
+    action_chosen_dice = [Action.CHOOSE_RAY, Action.CHOOSE_COW, Action.CHOOSE_HUMAN, Action.CHOOSE_CHICKEN]
 
     @classmethod
     def choose_roll(cls):
@@ -26,22 +25,22 @@ class Bot(PlayerInteraction):
     def choose_dices(cls, remaining_dice, chosen_dice):
         while True:
 
-            action = random.choice(cls.action_chouse_dice)
+            action = random.choice(cls.action_chosen_dice)
 
             if action == Action.CHOOSE_RAY and Dice.RAY in remaining_dice:
                 return action
 
             if (action == Action.CHOOSE_COW and
-                Dice.COW in remaining_dice and
-                Dice.COW not in chosen_dice):
-                    return action
+                    Dice.COW in remaining_dice and
+                    Dice.COW not in chosen_dice):
+                return action
 
             if (action == Action.CHOOSE_HUMAN and
-                Dice.HUMAN in remaining_dice and
-                Dice.HUMAN not in chosen_dice):
-                    return action
+                    Dice.HUMAN in remaining_dice and
+                    Dice.HUMAN not in chosen_dice):
+                return action
 
             if (action == Action.CHOOSE_CHICKEN and
-                Dice.CHICKEN in remaining_dice and
-                Dice.CHICKEN not in chosen_dice):
-                    return action
+                    Dice.CHICKEN in remaining_dice and
+                    Dice.CHICKEN not in chosen_dice):
+                return action
