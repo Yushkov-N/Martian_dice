470f6535944909c6a9bd55c5ef24e11294dce350
Yushkov-N <RTUniver@yandex.ru>
2024-12-03 21:38:56 +0300

    Исправлен roll(), откорректирован __repl__

--- src/dice.py
+++ src/dice.py
@@ -2,7 +2,6 @@ import random
 
 
 class Dice:
-
     TANK: str = 'tank'
     RAY: str = 'ray'
     HUMAN: str = 'human'
@@ -11,14 +10,19 @@ class Dice:
 
     VALUES = [TANK, RAY, RAY, HUMAN, COW, CHICKEN]
 
-    def __init__(self, values=VALUES[0]):
+    def __init__(self, values=VALUES[1]):
         self.side = values
 
     def __repr__(self):
-        return f'{self.side}'
+        return f'|{self.side}|'
 
     def __eq__(self, other_side):
         return self.side == other_side
+
+    def roll(self):
+        sides = self.all_sides()
+        roll_result = random.choice(sides)
+        return roll_result
 
     def save(self):
         return self.side
@@ -33,9 +37,3 @@ class Dice:
             sides = cls.VALUES
         sides = [cls(side) for side in sides]
         return sides
-
-    @classmethod
-    def roll(cls, count: int = 0):
-        sides = cls.VALUES
-        roll_result = [cls(random.choice(sides)) for _ in range(count)]
-        return roll_result
